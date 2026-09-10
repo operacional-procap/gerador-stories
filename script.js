8ab23b41632e0f97bbe033826f5f94f4a1a765a2
Operacional Procap <matheus@procapitalizacao.com.br>
2026-09-10 16:16:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -566,6 +566,22 @@ function gerarTodosOsPNGs() {
             fundo: "https://operacional-procap.github.io/gerador-stories/fundo_7digitos.png",
             arquivo: "7digitos.png",
             desenhar: desenhar7Digitos
+        },
+
+        {
+            fundo: "https://operacional-procap.github.io/gerador-stories/fundo_5digitos_incentivo.png",
+            arquivo: "5digitos_incentivo.png",
+            desenhar: desenhar5Digitos
+        },
+        {
+            fundo: "https://operacional-procap.github.io/gerador-stories/fundo_6digitos_incentivo.png",
+            arquivo: "6digitos_incentivo.png",
+            desenhar: desenhar6Digitos
+        },
+        {
+            fundo: "https://operacional-procap.github.io/gerador-stories/fundo_7digitos_incentivo.png",
+            arquivo: "7digitos_incentivo.png",
+            desenhar: desenhar7Digitos
         }
 
     ];
